7705524cb937bf3c0b031b7e2fdac5647f3216cb
Angel Vega <andresvegacordero007@gmail.com>
2021-02-10 22:37:36 -0600

    Ventana de respaldos, falta creacion de respaldo como tal y recuperacion de contraseña desde dentro del sistema

---
+++ ProyectoLithio/Controllers/RespaldosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoLithio.Controllers
+{
+    public class RespaldosController : Controller
+    {
+
+        [HttpPost]
+        public ActionResult CrearRespaldo()
+        {
+            string mensaje = "";
+            string titulo = "";
+            string icono = "";
+            try
+            {
+                mensaje = "Respaldo hecho de forma existosa!";
+                titulo = "Éxito";
+                icono = "success";
+
+            }
+            catch (Exception ex)
+            {
+                mensaje = "Hubo un error" + ex.Message;
+                titulo = "Hubo un error";
+                icono = "error";
+            }
+
+            return Json(
+                new { mensaje = mensaje, titulo = titulo,icono = icono}
+                );
+        }
+
+    }
+}
